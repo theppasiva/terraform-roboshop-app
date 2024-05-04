229d661ee02d53ff6f1bec1d69c82dfce25a5bc4
theppasiva <theppasiva@gmail.com>
2024-05-04 12:22:30 +0530

    terraform-roboshop-app

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "common_tags" {
+    # default = {
+    #     Project = "roboshop"
+    #     Environment = "dev"
+    #     Terraform = "true"
+    # }
+}
+
+variable "tags" {
+ # if default is not provide user must provide value
+}
+
+variable "project_name" {
+    #default = "roboshop"
+}
+
+variable "environment" {
+  #default = "dev"
+}
+
+variable "zone_name" {
+  #default = "shivarampractise.online"
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "component_sg_id" {
+  
+}
+
+variable "private_subnet_ids" {
+  
+}
+
+variable "iam_instance_profile" {
+  
+}
+
+variable "app_alb_listener_arn" {
+  
+}
